73646845d3a083a4c22c044e7f61e505f7302d2f
unknown <1starwarrior516@gmail.com>
2022-08-12 02:44:34 +0530

    initial commit

---
+++ deploy-commands.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId } = require("./config.json");
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+
+const token = process.env.BOT_TOKEN;
+
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+rest
+  .put(Routes.applicationCommands(clientId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
